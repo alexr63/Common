ad87aa1d7b24ace50ad40cff81867ee7c01cc800
alexr63 <alex@resnik.co.ua>
2013-12-31 15:50:18 +0200

    very first working version

---
+++ Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Utils
+    {
+        public static string TruncateAtWord(this string input, int length)
+        {
+            if (input == null || input.Length < length)
+                return input;
+            int iNextSpace = input.LastIndexOf(" ", length);
+            return String.Format("{0}...", input.Substring(0, (iNextSpace > 0) ? iNextSpace : length).Trim());
+        }
+
+        public static string GetCurrencySymbol(string currencyCode)
+        {
+            foreach (CultureInfo nfo in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+            {
+                RegionInfo region = new RegionInfo(nfo.LCID);
+                if (region.ISOCurrencySymbol == currencyCode)
+                {
+                    return region.CurrencySymbol;
+                }
+            }
+            return currencyCode;
+        }
+    }
+}
