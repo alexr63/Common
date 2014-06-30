78197bfc0ae5ff5432cc438a543f82628719435f
alexr63 <alex@resnik.co.ua>
2014-06-30 09:25:29 +0300

    MyLogger

---
+++ MyLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Services.Log.EventLog;
+
+namespace Common
+{
+    public class MyLogger
+    {
+        public static void Log(string logInfo, PortalSettings portalSettings)
+        {
+            EventLogController eventLog = new EventLogController();
+
+            eventLog.AddLog("ProductList", logInfo, portalSettings,
+                -1,
+                EventLogController.EventLogType.ADMIN_ALERT);
+        }
+    }
+}
