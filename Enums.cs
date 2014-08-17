76b2efc28ff1c4ba07ca30d6922de1f745b80fde
Alexander Resnik <resnik@sed.lg.ua>
2014-08-17 13:31:07 +0300

    add Enums.cs

---
+++ Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common
+{
+    public class Enums
+    {
+        public enum ProductTypeEnum
+        {
+            Hotels = 1,
+            Clothes,
+            Books,
+            HomeAndGardens,
+            Boats
+        }
+
+        public enum DisplayModeEnum
+        {
+            Hotels = 1,
+            Products,
+            Boats
+        }
+    }
+}
